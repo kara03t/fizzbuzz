611695954b4fba62572bcf4cdaf3fd4d3fb7f941
kara03t <tomoki-kara@hotmail.co.jp>
2021-05-10 21:01:06 +0900

    add minitest

--- fizz_buzz_test.rb
+++ fizz_buzz_test.rb
@@ -1,16 +1,5 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'fizz buzz'
-  elsif n % 3 == 0
-    'fizz'
-  elsif n % 5 == 0
-    'buzz'
-  else
-    n.to_s
-  end
-end
-
 require 'minitest/autorun'
+require_relative './fizzbuzz.rb'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
